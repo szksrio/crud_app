0452a4f6bb8b1c773a15a9e727a08badaa79e9e4
Samin <samin.khan@springrain.io>
2023-06-26 16:02:05 +0600

    koa cors added

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose')
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const router = require('./route');
-const db = require('./config/db');
+const mongoose = require("mongoose");
+const Koa = require("koa");
+const bodyParser = require("koa-bodyparser");
+const cors = require("koa2-cors");
+const router = require("./route");
+const db = require("./config/db");
 const port = 8080;
+
 const app = new Koa();
 
+app.use(cors({ origin: "*" }));
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 db();
-app.listen(port,()=>{
-    console.log(`Application has started at ${port}`);
+app.listen(port, () => {
+  console.log(`Application has started at ${port}`);
 });
-
